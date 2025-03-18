7b3a3a4658db73d69267134a67146c19ad79d663
marekgacek45 <marekgacek45@gmail.com>
2025-03-18 17:44:21 +0100

    update

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+	
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: process.env.DIRECTUS_API_ENDPOINT!.replace('https://', ''),
+			},
+		],
+	},
+}
+
+export default nextConfig
